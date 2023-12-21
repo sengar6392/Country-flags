7177b6fac12d8ad8fcf4d9ff214baaba7ae3f1ea
Shivam <shivamsengar995627@gmail.com>
2023-12-21 16:13:54 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [data, setData] = useState([]);
+  const fetchCountries = async () => {
+    try {
+      const res = await axios.get("https://restcountries.com/v3.1/all");
+      setData(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+  if (data.length)
+    return (
+      <div>
+        <h2 style={{padding:"1rem",textAlign:"center"}}>Country Flags</h2>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: "1rem",
+            padding: "1rem",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {data.map((country) => (
+            <div
+              key={country.name.official}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                border: "1px solid black",
+                height: "10rem",
+                width: "10rem",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              <div
+                style={{ height: "4rem", width: "4rem", overflow: "hidden" }}
+              >
+                <img
+                  src={country.flags.png}
+                  alt={country.name.common}
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    objectFit: "contain",
+                  }}
+                ></img>
+              </div>
+              <p>{country.name.common}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
 }
 
 export default App;
